27fe4bbbca7b8efa0107750705fcdf3d6c710160
andydockett <adockett@didigroup.com>
2013-08-13 00:17:36 -0400

    try agin

--- www/js/didi.js
+++ www/js/didi.js
@@ -38,7 +38,7 @@
 
                         // prevent scrolling
                         if (Math.abs(start.coords[1] - stop.coords[1]) > 10) {
-                            event.preventDefault();
+                           
                         }
                     }
 
